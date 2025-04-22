62ca79d34cdf103791999b88c3ddd06e9fbbba43
ma-lay <malaymuskan529@gmail.com>
2025-04-22 23:24:39 +0530

    UserService

--- backend/src/main/java/com/boneai/backend/service/UserService.java
+++ backend/src/main/java/com/boneai/backend/service/UserService.java
@@ -1,5 +1,23 @@
 package com.boneai.backend.service;
 
+
+import com.boneai.backend.entity.User;
+import com.boneai.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
-    
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
 }
