282c3638b2cb7c727e2109319326a8a64adcc9bb
ma-lay <malaymuskan529@gmail.com>
2025-04-22 23:23:26 +0530

    UserController

--- backend/src/main/java/com/boneai/backend/controller/UserController.java
+++ backend/src/main/java/com/boneai/backend/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.boneai.backend.controller;
 
+import com.boneai.backend.entity.User;
+import com.boneai.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/save")
+    public User saveUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/email/{email}")
+    public Optional<User> getUserByEmail(@PathVariable String email) {
+        return userService.getUserByEmail(email);
+    }
 }
