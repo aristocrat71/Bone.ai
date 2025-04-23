1c67c1de672ce5b32b954f3edbd6dd5f2edce412
k-arsyn <sengarkartikeya028@gmail.com>
2025-04-23 09:11:30 +0530

    auth-preset2

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,9 +1,4 @@
-// utils/auth.js
 export const getLoggedInUsername = () => {
-    // For now, return a dummy value
     return "Test User";
-  
-    // Later, you can fetch from context/localStorage, e.g.:
-    // return localStorage.getItem("username");
   };
   
