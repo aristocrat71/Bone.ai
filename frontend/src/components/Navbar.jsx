9676cfc280a3f43f2ecc0a037cbb044ccce14537
k-arsyn <sengarkartikeya028@gmail.com>
2025-04-23 09:10:06 +0530

    navbar-set

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav style={{ backgroundColor: '#1976d2', padding: '10px', color: 'white' }}>
+      <h3>Bone.ai</h3>
+    </nav>
+  );
+};
+
+export default Navbar;
