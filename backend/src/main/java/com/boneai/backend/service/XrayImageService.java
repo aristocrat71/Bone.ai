a1c927438559c1dceb0341ccd53bd938e8624861
k-arsyn <sengarkartikeya028@gmail.com>
2025-04-24 10:25:57 +0530

    xray-image-service

--- backend/src/main/java/com/boneai/backend/service/XrayImageService.java
+++ backend/src/main/java/com/boneai/backend/service/XrayImageService.java
@@ -1,5 +1,22 @@
 package com.boneai.backend.service;
 
+import com.boneai.backend.entity.XrayImage;
+import com.boneai.backend.repository.XrayImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class XrayImageService {
-    
+    @Autowired
+    private XrayImageRepository xrayImageRepository;
+
+    public XrayImage saveImage(XrayImage image) {
+        return xrayImageRepository.save(image);
+    }
+
+    public Optional<XrayImage> getImageById(Long id) {
+        return xrayImageRepository.findById(id);
+    }
 }
