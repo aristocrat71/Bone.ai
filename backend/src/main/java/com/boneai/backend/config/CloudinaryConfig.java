c751ed90b205cfbf43ba9c9aa108c950d4f09e04
k-arsyn <sengarkartikeya028@gmail.com>
2025-04-25 09:09:09 +0530

    :(

--- backend/src/main/java/com/boneai/backend/config/CloudinaryConfig.java
+++ backend/src/main/java/com/boneai/backend/config/CloudinaryConfig.java
@@ -5,16 +5,4 @@ import com.cloudinary.utils.ObjectUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class CloudinaryConfig {
 
-    @Bean
-    public Cloudinary cloudinary() {
-        return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "dtchcrwn9",
-                "api_key", "224466977721211",
-                "api_secret", "WBqAry5UJrCuCHGza_1M2xGSjSs",
-                "secure", true
-        ));
-    }
-}
