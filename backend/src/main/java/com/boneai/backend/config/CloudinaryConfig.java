4fad8da1e55a7767fa30a379f0b1d39e55972ff2
k-arsyn <sengarkartikeya028@gmail.com>
2025-04-25 07:57:20 +0530

    cloudinary-cloud-storage-configuration-bean-added

--- backend/src/main/java/com/boneai/backend/config/CloudinaryConfig.java
+++ backend/src/main/java/com/boneai/backend/config/CloudinaryConfig.java
@@ -1,5 +1,20 @@
 package com.boneai.backend.config;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class CloudinaryConfig {
-    
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", "dtchcrwn9",
+                "api_key", "224466977721211",
+                "api_secret", "WBqAry5UJrCuCHGza_1M2xGSjSs",
+                "secure", true
+        ));
+    }
 }
