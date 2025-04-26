f9d556f1c345f74b9b8e97d2bff899cc675e0d17
ma-lay <malaymuskan529@gmail.com>
2025-04-26 20:02:40 +0530

    report repo

--- backend/src/main/java/com/boneai/backend/repository/ReportRepository.java
+++ backend/src/main/java/com/boneai/backend/repository/ReportRepository.java
@@ -1,4 +1,10 @@
 package com.boneai.backend.repository;
 
-public class ReportRepository {
+import com.boneai.backend.entity.Report;
+import com.boneai.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+    List<Report> findByUser(User user);
 }
