bef7b5bee61946057a08e5056399269214a8cb3c
k-arsyn <sengarkartikeya028@gmail.com>
2025-04-26 09:16:45 +0530

    report-service-added

--- backend/src/main/java/com/boneai/backend/service/ReportService.java
+++ backend/src/main/java/com/boneai/backend/service/ReportService.java
@@ -1,5 +1,31 @@
 package com.boneai.backend.service;
 
+import com.boneai.backend.entity.Report;
+import com.boneai.backend.entity.User;
+import com.boneai.backend.repository.ReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ReportService {
-    
+    @Autowired
+    private ReportRepository reportRepository;
+
+    public Report generateReport(Report report) {
+        return reportRepository.save(report);
+    }
+
+    public Optional<Report> getReportById(Long id) {
+        return reportRepository.findById(id);
+    }
+
+    public List<Report> getReportsByUser(Long userId) {
+        User user = new User();
+        user.setUserId(userId);
+        return reportRepository.findByUser(user);
+
+    }
 }
